77a37e2d7e4883faec6956060590ed9a95430b9c
carriegale2710 <carriegale2710@gmail.com>
2025-04-03 13:33:59 +1100

    as letter button amount

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 
 // This task will require you to create a web based game of [Hangman](https://en.wikipedia.org/wiki/Hangman_\(game\)) using JavaScript.
 
+
 // This will require understanding of:
 
 // - [[Array]]s
@@ -12,22 +13,29 @@
 // -  [[Event-listeners]]
 // - [[string-manipulation]]
 
-// There are images provided for each stage of the eleven guesses, but feel free to use your own if you like. There is a word list provided, feel free to modify it as you see fit.
+// There are images provided for each stage of the eleven guesses, but feel free to use your own if you like. 
+// There is a word list provided, feel free to modify it as you see fit.
 
-// ## MVP
+//NOTE -  ## MVP
 
 // A game should start with a word being randomly selected.
-// A representation of the word made up of underscore (_) characters should display on the screen.
-// Create a user interface made up of buttons representing all 26 letters of the alphabet.
+//// A representation of the word made up of underscore (_) characters should display on the screen.
+//// Create a user interface made up of buttons representing all 26 letters of the alphabet.
 // Clicking a button should register its letter as a 'guess'
 // If the letter clicked is in the word each underscore corresponding to that letter should be replaced with the letter.
 // If the letter is not in the word, an additional element to the hangman diagram should be added.
 // The game should keep track of which letters have been 'guessed' already.
+    //idea- hide the button after selected
 // The game should provide a win or loss message depending on the outcome
 // There should be some functionality to play again.
 //// This should be styled to support mobile, tablet, and desktop views 
 
-// ## Bonus
+//NOTE -  ## Bonus
 
 // Allow keyboard input
 // Keep track of words that have been used already and display them on the screen
+
+
+//!SECTION
+
+//there are eleven guesses
