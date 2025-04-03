2c63644a2102e16f464b868d860772c7dde0f08b
carriegale2710 <carriegale2710@gmail.com>
2025-04-03 16:00:21 +1100

    using pseudocode to break down the problem

--- script.js
+++ script.js
@@ -18,17 +18,39 @@
 
 //NOTE -  ## MVP
 
+//Styling, HTML Elements:
+    //// A representation of the word made up of underscore (_) characters should display on the screen.
+    //// Create a user interface made up of buttons representing all 26 letters of the alphabet.
+    //// This should be styled to support mobile, tablet, and desktop views 
+
+//Functionality:
+
 // A game should start with a word being randomly selected.
-//// A representation of the word made up of underscore (_) characters should display on the screen.
-//// Create a user interface made up of buttons representing all 26 letters of the alphabet.
+
 // Clicking a button should register its letter as a 'guess'
-// If the letter clicked is in the word each underscore corresponding to that letter should be replaced with the letter.
+    //remove the letter button that was pressed
+    //sends the letter to a function called CheckLetterExistsinWord()
+        // probably a every() iterator, will return true or false for each element checked
+            //if true - replace underscore with the letter, DO NOT increment img number
+            //if false - DO increase increment of img number name to change image to next one
+
 // If the letter is not in the word, an additional element to the hangman diagram should be added.
+    //there are eleven guesses or 'lives' - so need to keep track of how many INCORRECT guesses have been made, and update the image to the next if an incorrect guess is made (event listener?)
+
+// If the letter clicked is in the word each underscore corresponding to that letter should be replaced with the letter.
+    //match letter to word[i], and replace in string array?
+
 // The game should keep track of which letters have been 'guessed' already.
     //idea- hide the button after selected
+
 // The game should provide a win or loss message depending on the outcome
+    //"GAME OVER - Try again?"
+    //"GAME WON - Congrats!"
+
 // There should be some functionality to play again.
-//// This should be styled to support mobile, tablet, and desktop views 
+    //refresh button appears along with message
+    //"NEW GAME"
+
 
 //NOTE -  ## Bonus
 
@@ -38,4 +60,6 @@
 
 //!SECTION
 
-//there are eleven guesses
+// A game should start with a word being randomly selected.
+    // access the words from json array?
+    // 
